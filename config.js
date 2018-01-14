e880ed7d857aa731e7ec5a70fc05c0057c67fddd
saikat chakrabortty <saikatchakrabortty2@gmail.com>
2018-01-14 13:08:47 +0530

    updated gitignore, and config added

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    secret: "SeCrEtKeYfOrHaShInG",
+    mongodbUri: "mongodb://localhost:27017/userdb",
+    //get this below credentials from here:  https://apps.twitter.com/
+    consumer_key: "xxxxxxxxxx",
+    consumer_secret: "xxxxxxxxx",
+    access_token_key: "xxxxx-xxxxxxx",
+    access_token_secret: "xxxxxxx"
+};
