67bc6995777b79e2678795c2c9f03e523eafb14f
saikat chakrabortty <saikatchakrabortty2@gmail.com>
2018-01-14 12:58:46 +0530

    updated with cluster

--- app.js
+++ app.js
@@ -1,81 +1,85 @@
-/* =======================
-    LOAD THE DEPENDENCIES
-==========================*/
-const express = require("express");
-const bodyParser = require("body-parser");
-const morgan = require("morgan");
-const mongoose = require("mongoose");
-const app = express();
-var server = require("http").createServer(app);
-const twitter = require("twitter");
-const io = require("socket.io").listen(server);
-const authMiddleware = require("./middlewares/auth");
-var tweet = io.of("tweet");
-var twit = new twitter({
-    consumer_key: config.consumer_key,
-    consumer_secret: config.consumer_secret,
-    access_token_key: config.access_token_key,
-    access_token_secret: config.access_token_secret
-});
+// Include the cluster module
+var cluster = require("cluster");
+// Code to run if we're in the master process
+if (cluster.isMaster) {
+    // Count the machine's CPUs
+    var cpuCount = require("os").cpus().length;
 
-/* =======================
-    LOAD THE CONFIG
-==========================*/
-const config = require("./config");
-const port = process.env.PORT || 3000;
+    // Create a worker
+    for (var i = 0; i < cpuCount; i += 1) {
+        cluster.fork();
+    }
+} else {
+    // Code to run if we're in a worker process
+    const express = require("express");
 
-/* =======================
-    EXPRESS CONFIGURATION
-==========================*/
+    const bodyParser = require("body-parser");
+    const morgan = require("morgan");
+    const mongoose = require("mongoose");
+    const app = express();
+    var server = require("http").createServer(app);
+    const twitter = require("twitter");
+    const io = require("socket.io").listen(server);
+    const authMiddleware = require("./middlewares/auth");
+    var tweet = io.of("tweet");
+    var twit = new twitter({
+        consumer_key: config.consumer_key,
+        consumer_secret: config.consumer_secret,
+        access_token_key: config.access_token_key,
+        access_token_secret: config.access_token_secret
+    });
+    const config = require("./config");
+    const port = process.env.PORT || 3000;
 
-function handler(req, res) {
-    twit.stream(
-        "statuses/filter",
-        { track: req.query.key || "javascript" },
-        function(stream) {
-            stream.on("data", function(data) {
-                io.sockets.emit("tweet", data.text);
-                console.log(".");
-            });
-        }
-    );
+    function handler(req, res) {
+        twit.stream(
+            "statuses/filter",
+            { track: req.query.key || "listen" },
+            function(stream) {
+                stream.on("data", function(data) {
+                    io.sockets.emit("tweet", data.text);
+                    console.log("Got a shake .");
+                });
+            }
+        );
+        //send the view
+        return res.sendFile(__dirname + "/client.html");
+    }
 
-    return res.sendFile(__dirname + "/client.html"); //sendthe view
-}
+    // parse JSON and url-encoded query
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
 
-// parse JSON and url-encoded query
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+    // print the request log on console
+    app.use(morgan("dev"));
 
-// print the request log on console
-app.use(morgan("dev"));
+    // set the secret key variable for jwt
+    app.set("jwt-secret", config.secret);
 
-// set the secret key variable for jwt
-app.set("jwt-secret", config.secret);
+    // index page, just for testing
+    app.get("/", (req, res) => {
+        res.send("Hi there!");
+    });
 
-// index page, just for testing
-app.get("/", (req, res) => {
-    res.send("Hi there!");
-});
+    //authenticate from route, i forgot
+    app.use("/api/v1", authMiddleware);
 
-//authenticate from route, i forgot
-app.use("/api/v1", authMiddleware);
+    app.get("/api/v1/client", handler);
 
-app.get("/api/v1/client", handler);
+    // configure api router
+    app.use("/api", require("./routes/api"));
 
-// configure api router
-app.use("/api", require("./routes/api"));
-
-// open the server
-server.listen(port);
-
-/* =======================
+    // open the server
+    server.listen(port);
+    console.log("Worker %d running!", cluster.worker.id);
+    /* =======================
     CONNECT TO MONGODB SERVER
-==========================*/
-mongoose.connect(config.mongodbUri);
+    ==========================*/
+    mongoose.connect(config.mongodbUri);
 
-const db = mongoose.connection;
-db.on("error", console.error);
-db.once("open", () => {
-    console.log("connected to mongodb server");
-});
+    const db = mongoose.connection;
+    db.on("error", console.error);
+    db.once("open", () => {
+        console.log("connected to mongodb server");
+    });
+}
